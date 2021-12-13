d67cc9783a179c95e7ff84108ae2f5e96255442e
suryanidewi32 <76469807+suryanidewi32@users.noreply.github.com>
2021-12-13 20:38:48 +0700

    modify redux-toolkit

--- src/Controllers/Services/Users/module_user.service.js
+++ src/Controllers/Services/Users/module_user.service.js
@@ -1,62 +1,63 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import UserService from "./module.service";
 
-const Createdata = "Createdata";
-const Retrive = "Retrive";
-const Update = "Update";
-const Delete = "Delete";
-  
-  export const createUser = (name, username, email, password, role, id) => async (dispatch) => {
-    try {
-      const res = await UserService.create({ name, username, email, password, role, id });
-  
-      dispatch({
-        type: Createdata,
-        payload: res.data,
-      });
-  
-      return Promise.resolve(res.data);
-    } catch (err) {
-      return Promise.reject(err);
-    }
-  };
-  
-  export const retrieveUser = () => async (dispatch) => {
-    try {
-      const res = await UserService.getAll();
-  
-      dispatch({
-        type: Retrive,
-        payload: res.data,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  
-  export const updateUser = (id, data) => async (dispatch) => {
-    try {
-      const res = await UserService.update(id, data);
-  
-      dispatch({
-        type: Update,
-        payload: data,
-      });
-  
-      return Promise.resolve(res.data);
-    } catch (err) {
-      return Promise.reject(err);
-    }
-  };
-  
-  export const deleteUser = (id) => async (dispatch) => {
-    try {
-      await UserService.remove(id);
-  
-      dispatch({
-        type: Delete,
-        payload: { id },
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
+const initialState = [];
+
+export const createUser = createAsyncThunk(
+  "users/create",
+  async ({ name, username, email, password, role, id }) => {
+    const res = await UserService.create({ name, username, email, password, role, id });
+    return res.data;
+  }
+);
+
+export const retrieveUser = createAsyncThunk(
+  "users/retrieve",
+  async () => {
+    const res = await UserService.getAll();
+    return res.data;
+  }
+);
+
+export const updateUser = createAsyncThunk(
+  "users/update",
+  async ({ id, data }) => {
+    const res = await UserService.update(id, data);
+    return res.data;
+  }
+);
+
+export const deleteUser = createAsyncThunk(
+  "users/delete",
+  async ({ id }) => {
+    await UserService.delete(id);
+    return { id };
+  }
+);
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  extraReducers: {
+    [createUser.fulfilled]: (state, action) => {
+      state.push(action.payload);
+    },
+    [retrieveUser.fulfilled]: (state, action) => {
+      return [...action.payload];
+    },
+    [updateUser.fulfilled]: (state, action) => {
+      const index = state.findIndex(user => user.id === action.payload.id);
+      state[index] = {
+        ...state[index],
+        ...action.payload,
+      };
+    },
+    [deleteUser.fulfilled]: (state, action) => {
+      let index = state.findIndex(({ id }) => id === action.payload.id);
+      state.splice(index, 1);
+    },
+  },
+});
+
+const { reducer } = userSlice;
+export default reducer;
